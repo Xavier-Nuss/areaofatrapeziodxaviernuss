767e07e7245ac7a4bbbca60f422f497cff12d42e
Xavier-Nuss <xavier.nuss@stu.ocsb.ca>
2026-05-05 13:52:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+let Base1 = game.askForNumber("The top base length of the trapezoid")
+let Base2 = game.askForNumber("\"The bottom base length of the trapezoid\"")
+let Height = game.askForNumber("\"The height of the trapezoid\"")
+let area = (Base1 + Base2) / 2
+game.splash(area)

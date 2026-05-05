4fb3d432aeddc094ef0a4fefe3f22f3379c40121
Xavier-Nuss <xavier.nuss@stu.ocsb.ca>
2026-05-05 14:00:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let Base1 = game.askForNumber("The top base length of the trapezoid")
 let Base2 = game.askForNumber("\"The bottom base length of the trapezoid\"")
 let Height = game.askForNumber("\"The height of the trapezoid\"")
-let area = (Base1 + Base2) / 2
-game.splash(area)
+let area = (Base1 + Base2) / 2 * Height
+game.splash("The area of the trapezoid is", area)
